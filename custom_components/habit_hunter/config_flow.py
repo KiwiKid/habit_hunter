88b8be16c31f682c8cb55a12edae55055b1af59d
KiwiKid <KiwiKid@users.noreply.github.com>
2023-09-06 09:17:09 +1200

    add list to config

--- custom_components/habit_hunter/config_flow.py
+++ custom_components/habit_hunter/config_flow.py
@@ -3,6 +3,7 @@ import voluptuous as vol
 from homeassistant import config_entries
 from homeassistant.core import callback
 from homeassistant.helpers.aiohttp_client import async_create_clientsession
+from homeassistant.helpers import config_validation as cv
 
 from .api import HabitHunterApiClient
 from .const import CONF_PASSWORD
@@ -53,8 +54,17 @@ class HabitHunterFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
         """Show the configuration form to edit location data."""
         return self.async_show_form(
             step_id="user",
-            data_schema=vol.Schema(
-                {vol.Required(CONF_USERNAME): str, vol.Required(CONF_PASSWORD): str}
+            data_schema=vol.Schema({
+                vol.Required(CONF_USERNAME): str
+                , vol.Required(CONF_PASSWORD): str
+                , vol.Optional('objects_list', default=[]): vol.All([
+                    vol.Schema({
+                        vol.Required('name'): str,
+                        vol.Required('reset_value'): int,
+                        vol.Required('reset_unit'): vol.In(['days', 'months', 'years'])
+                })
+            ])
+                }
             ),
             errors=self._errors,
         )
